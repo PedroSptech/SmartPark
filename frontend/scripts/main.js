dc4886080ae0b10cd371b949854374e7db8cdd3a
PedroSptech <pedro.nlima@sptech.school>
2026-04-12 20:52:24 -0300

    feat: finalizing the register page

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -4,28 +4,35 @@ const usuario = {
   password: "jeremy@123"
 }
 
-// function registerUser() {
-//   const name = ipt_nome.value
-//   const email = ipt_email.value
-//   const passw = ipt_passw.value
-//   const c_passw = ipt_confirm_passw.value
+function registerUser() {
+  const name = ipt_nome.value
+  const email = ipt_email.value
+  const passw = ipt_passw.value
+  const c_passw = ipt_confirm_passw.value
 
-//   if(passw !== c_passw) {
-//     invalidPassword()
-//     return
-//   }
+  if (name == "" || email == "" || passw == "" || c_passw == "") {
+    alert("Preencha todos os campos")
+    return
+  }
 
-//   const user = {
-//     ids,
-//     name,
-//     email,
-//     passw
-//   }
+  if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+    alert("Email inválido")
+    return
+  }
 
-//   users.push(user)
+  if (passw.length < 6) {
+    alert("Senha muito curta")
+    return
+  }
 
-//   location.href = "../pages/login.html" 
-// }
+  if (passw !== c_passw) {
+    alert("As senhas não coincidem")
+    return
+  }
+
+  alert("Cadastro realizado com sucesso!")
+  window.location.href = "./login.html"
+}
 
 function loginUser() {
   const email = ipt_email.value
@@ -36,11 +43,9 @@ function loginUser() {
     passw
   }
 
-  if(user.email == usuario.email && user.passw == usuario.password) {
-    //TODO: MUDAR PARA A USER PAGE / DASHBOARD
-    alert("Logado") 
-    return
+  if (user.email == usuario.email && user.passw == usuario.password) {
+    window.location.href = "./dashboard.html";
+  } else {
+    alert("Email ou senha inválidos");
   }
-
-  alert("Email ou senha invalidos")  
 }
