f04da60e771c1d98f6edc375092655ad0ffaa1f6
Lucas Moraes <lucas.moraes@sptech.school>
2026-05-18 13:10:30 -0300

    Feat: Switch page de registro

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -11,15 +11,17 @@ const listaUsuarios = [
     }
 ];
 
-function switchTab(tab) {
+
+//TODO: EXPLICAR CODE
+function switchTab(tab, type) {
     if (tab === 'empresa') {
         formFuncionario.style.display = 'none';
-        formEmpresa.style.display = 'block';
+        formEmpresa.style.display = type == 1 ? 'block' : 'flex'
         tabEmpresa.classList.add('tab-active');
         tabFuncionario.classList.remove('tab-active');
     } else {
         formEmpresa.style.display = 'none';
-        formFuncionario.style.display = 'block';
+        formFuncionario.style.display = type == 1 ? 'block' : 'flex'
         tabFuncionario.classList.add('tab-active');
         tabEmpresa.classList.remove('tab-active');
     }
