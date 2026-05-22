a29ee90e19b4a718ea9760d1853cf00669cb3f3e
PedroSptech <pedro.nlima@sptech.school>
2026-05-21 21:01:03 -0300

    feat: add the new BobIA backend

--- backend/src/models/bobiaModel.js
+++ backend/src/models/bobiaModel.js
@@ -4,13 +4,13 @@ const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
 
 async function gerarResposta(mensagem) {
      try {
-        const modeloIA = chatIA.models.generateContent({
+        const modeloIA = await chatIA.models.generateContent({
             model: "gemini-2.5-flash",
             contents: `${mensagem}`
 
         });
-        const resposta = (await modeloIA).text;
-        const tokens = (await modeloIA).usageMetadata;
+        const resposta = modeloIA.text;
+        const tokens = modeloIA.usageMetadata;
 
         console.log(resposta);
         console.log("Uso de Tokens:", tokens);
